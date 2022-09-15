611531516d8cc5a0464c61bd04b64080ee770cf5
seowlee <seowlee@iseouui-MacBookPro.local>
2022-09-15 18:59:45 +0900

    FEAT: 스프링 DB 접근기술 - H2 DB, JDBC, 스프링 통합테스트, 스프링 JdbcTemplate, JPA

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,21 +1,47 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-	@Bean
-	public MemberService memberService() {
-		return new MemberService(memberRepository());
-	}
-	
-	@Bean
-	public MemberRepository memberRepository() {
-		return new MemoryMemberRepository();
-	}
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    private EntityManager em;
+
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
+    }
+//    private final MemberRepository memberRepository;
+//
+//    public SpringConfig(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
+
+    }
 }
